40b8085d1e8e39115b9677b1b6354c19e8fbe8d2
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-20 12:51:06 +0200

    methods ToLower() and ToUpper()

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.substrings
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = "PesHo";
+            Console.WriteLine(text);
+            text =
+                 text.ToLower(); //take teh opls text string and assigns the new all low Symbols! 
+            Console.WriteLine(text); //resutl all is low cased !!!
+            text = text.ToUpper(); //takes the text and converts it to upperCase all!
+            Console.WriteLine(text);
+        }
+    }
+}
